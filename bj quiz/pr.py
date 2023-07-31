26d059826fa9b8032c1f2c693817122a437821eb
HOONTP <dltkdgns0303@gmail.com>
2023-07-31 16:39:24 +0900

    sdfs

--- bj quiz/pr.py
+++ bj quiz/pr.py
@@ -4,11 +4,22 @@ input = sys.stdin.readline
 start_time = time.time()
 # N = int(input())
 
-last_char = '3'
-if '20' <= last_char <= '10':
-    print(True)
-else:
-    print(False)
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    V = list(map(int, input().split()))
+    i = 0
+    lst = []
+    for i in range(N-M+1):
+        sm = 0
+        it = 0
+        while it < M:
+            sm += V[i+it]
+        lst.append(sm)
+    lst.sort()
+    print(f'#{T} {lst[-1]-lst[0]}')
+
 
 end_time = time.time()
 print("time:", end_time - start_time)

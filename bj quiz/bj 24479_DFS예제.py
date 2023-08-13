d8537d90eda2d436c35e8312baa4dcb4911a7f12
HOONTP <dltkdgns0303@gmail.com>
2023-08-13 17:56:01 +0900

    awefawef

---
+++ bj quiz/bj 24479_DFS예제.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(V, E, R):
+    global result
+    visted = [0] * (V+1)
+    stack = []
+    visted[R] = 1
+    result[R] = 1
+    i = 2
+
+    while True:
+        for w in E[R]:
+            if visted[w] == 0:
+                stack.append(R)
+                R = w
+                visted[R] = 1
+                result[R] = i
+                i += 1
+                break
+        else:
+            if stack:
+                R = stack.pop()
+            else:
+                break
+    return
+
+N, M, R = map(int, input().split())
+arr = [[] for _ in range(N+1)]
+result = {}
+
+for __ in range(M):
+    A, B = map(int, input().split())
+    arr[A].append(B)
+    arr[B].append(A) # 이거 안해서 몇분을 쳐다 본거야.?
+
+for i in arr:#왜 시간복잡도 ㅜ
+    if len(i) > 1:
+        i.sort()
+
+if len(arr[1]) == M:
+    print(1)
+    for i in arr[1]:
+        print(i)
+else:
+    dfs(N, arr, R)
+    for i in range(1, N+1):
+        print(result.get(i, 0))
--- bj quiz/bj 24479_복사본 분석하기.py
+++ bj quiz/bj 24479_복사본 분석하기.py
@@ -10,12 +10,13 @@ def main():
     order = 1
     while stack:
         print(stack) #다 넣었으니까 뒤에서부터 뺸다.
-        cur = stack.pop()#99를 빼고
-        if visited[cur]:#[99]는 0이니까 진행
+        cur = stack.pop()#제일 마지막에 들어간 수를 빼고
+        if visited[cur]:#아직 안들어간 곳이면 진행
             continue
-        visited[cur] = order#99에 2를 부여? 왜?
+        visited[cur] = order#99에 2를 부여? 왜? 아니었죠 / 들어갔으니 다음 번호 부여
         order += 1
         for nex in sorted(graph[cur], reverse=True):#만약 99부터 2까지 들어갔따치자.
+            # 새로 들어온 곳 역순으로 다 넣음. 방문한 곳은 제외.
             if not visited[nex]:#0이면 트루라서 진행
                 stack.append(nex)
     stdout.write("\n".join(map(str, visited[1:])))

12cdbc81cda4850beaed5a8162072f53cd6bb613
HOONTP <dltkdgns0303@gmail.com>
2023-08-13 15:12:27 +0900

    hollys

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -3,20 +3,12 @@ input = sys.stdin.readline
 
 N = int(input())
 
-lst = [int(input()) for _ in range(N)]
+lst = list(map(int, input().split()))
+result = [1]*N
 
-result = [0]*N
-result[0] = lst[0]
+for i in range(N):
+    for j in range(i):
+        if lst[i] > lst[j]:
+            result[i] = max(result[i], result[j]+1)
 
-for i in range(1, N):
-    if i-3 >= 0:
-        new_sum = result[i-3]+lst[i-1]+lst[i]
-    else:
-        new_sum = lst[i-1]+lst[i]
-    if i-2 >= 0:
-        sec_sum = result[i-2] + lst[i]
-    else:
-        sec_sum = lst[i]
-
-    result[i] = max(result[i-1], new_sum, sec_sum)
-print(result[-1])
+print(max(result))

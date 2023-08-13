fe4df7205edaacfa94f601cf992bebdb0dc718d5
HOONTP <dltkdgns0303@gmail.com>
2023-08-14 00:12:26 +0900

    sleep

--- bj quiz/bj 9251_LCS공통문자수열.py
+++ bj quiz/bj 9251_LCS공통문자수열.py
@@ -4,12 +4,27 @@ input = sys.stdin.readline
 A = input().strip()
 B = input().strip()
 
-N_A = len(A)
 N_B = len(B)
 result = [0] * (N_B+1)
-for i in range(N_A):
-    for j in range(N_B-1, -1, -1):
-        if A[i] == B[j]:
-            result[j+1] = max(result[:j+1]) + 1
 
-print(max(result))
+if A == B: # 겹치는 부분 추가하고 시간 맞춤
+    print(N_B)
+elif B in A:
+    print(N_B)
+elif A in B:
+    print(len(A))
+else:
+    for i in A:
+        # print(result)
+        for j in range(N_B-1, -1, -1):
+            if i == B[j]:
+                result[j+1] = max(result[:j+1]) + 1
+
+    print(max(result))
+
+# for i in range(N_A): #시간초과ㅜㅜㅜㅜ 93퍼
+#     for j in range(N_B-1, -1, -1):
+#         if A[i] == B[j]:
+#             result[j+1] = max(result[:j+1]) + 1
+#
+# print(max(result))
--- pythonP/algo.py
+++ pythonP/algo.py
@@ -4,12 +4,20 @@ input = sys.stdin.readline
 A = input().strip()
 B = input().strip()
 
-N_A = len(A)
 N_B = len(B)
 result = [0] * (N_B+1)
-for i in range(N_A):
-    for j in range(N_B-1, -1, -1):
-        if A[i] == B[j]:
-            result[j+1] = max(result[:j+1]) + 1
 
-print(max(result))
+if A == B:
+    print(N_B)
+elif B in A:
+    print(N_B)
+elif A in B:
+    print(len(A))
+else:
+    for i in A:
+        # print(result)
+        for j in range(N_B-1, -1, -1):
+            if i == B[j]:
+                result[j+1] = max(result[:j+1]) + 1
+
+    print(max(result))

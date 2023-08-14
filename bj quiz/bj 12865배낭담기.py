bba762a7c94684fce85409d07980dc5b3df6c91e
HOONTP <dltkdgns0303@gmail.com>
2023-08-14 15:43:05 +0900

    rgb

--- bj quiz/bj 12865배낭담기.py
+++ bj quiz/bj 12865배낭담기.py
@@ -9,7 +9,7 @@ for _ in range(N):
 arr.sort()
 # print(arr)
 for i in arr:
-    for j in range(K, -1, -1):
+    for j in range(K, -1, -1):#뒤에서 부터 해야함!
         if i[0] + j < K+1:
             count[i[0] + j] = max(i[1]+count[j], count[i[0] + j])
     # print(count)

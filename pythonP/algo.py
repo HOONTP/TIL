c53628d6bd9267256be803384b6eef9d09a64044
HOONTP <dltkdgns0303@gmail.com>
2023-08-17 00:05:18 +0900

    mmmmmeeeeeeeeeeeeeooeoeoeoeo

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -10,13 +10,7 @@ for _ in range(T):
     result = [0]*N
     mid = [0]*N
     print(lst)
-
-    mid[0] = result[0] = lst[0]
-    mid[1] = result[1] = lst[0]+lst[1]
-
-    for i in range(2, N):
-        result[i] = min((result[i-1] + mid[i-1] + lst[i]), (result[i-2] + mid[i-2] + 2*(lst[i-1]+lst[i])))
-        mid[i] = mid[i-1] + lst[i]
+#현재 값보다 다음 연산이 작을 경우
 
     print(mid, 'md')
     print(result, 're')

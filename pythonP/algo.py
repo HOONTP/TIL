89b0e9f90614388b6cd460dcbd6977e121b6b61e
HOONTP <dltkdgns0303@gmail.com>
2023-08-18 10:20:36 +0900

    bfs

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,30 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-def backT(visted, index):
+def backT(visted, n):
     if len(visted) == M:
         result.add(tuple(visted))#왜 튜플만 되는겨 여튼 튜플만 set에 가능
         return
 
     for i in range(N):
-        if i not in index:
+        if i >= n:
             visted.append(lst[i])
-            index.append(i)
-            backT(visted, index)
+            backT(visted, i)
             visted.pop()
-            index.pop()
-
-
 
 N, M = map(int, input().split())
 
 lst = list(map(int, input().split()))
 visted = []
-index = []
 result = set()
 lst.sort()
 
-backT(visted, index)
+backT(visted, 0)
 result = sorted(result) #set는 순서가 없단다 친구야
 for _ in result:
     print(*_)

602aa43c5e609acd9ee318a1ccd5412a27ed78ce
HOONTP <dltkdgns0303@gmail.com>
2023-08-31 17:57:27 +0900

    ssafy

--- bj quiz/bj 12865배낭담기.py
+++ bj quiz/bj 12865배낭담기.py
@@ -10,7 +10,7 @@ arr.sort()
 # print(arr)
 for i in arr:
     for j in range(K, -1, -1):#뒤에서 부터 해야함!
-        if i[0] + j < K+1:
-            count[i[0] + j] = max(i[1]+count[j], count[i[0] + j])
+        if i[0] + j < K+1:    # i의 무게와 더 할 수 있는 무게의 index에 아래 값을 넣자.
+            count[i[0] + j] = max(i[1]+count[j], count[i[0] + j]) # i[1]와 더 할 수 있는 j의 값을 더한 값과, 현재의 값을 비교
     # print(count)
 print(max(count))
--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,27 +1,16 @@
-import sys
-input = sys.stdin.readline
+N, K = map(int, input().split())
 
-N = int(input())
-lst = list(map(int, input().split()))
+count = [0]*(K+1)
+arr = []
+for _ in range(N):
+    w, v = map(int, input().split())
+    arr.append((w,v))
 
-# 100만개 100번 이내로 순회 해야되는데 ? 중 복 불 가 음  /  2회 순회?
-
-stack = [[0,lst[0]]]
-result = [0 for _ in range(N)]
-counts = [0 for _ in range(1000001)]
-
-for i in lst:
-    counts[i] += 1
-
-for i in range(1, N):
-    if counts[lst[i]] <= counts[stack[-1][1]]:
-        stack.append([i, lst[i]])
-    if counts[lst[i]] > counts[stack[-1][1]]:
-        while stack and counts[lst[i]] > counts[stack[-1][1]]:
-            result[stack[-1][0]] = lst[i]
-            stack.pop()
-        stack.append([i, lst[i]])
-while stack:
-    result[stack[-1][0]] = -1
-    stack.pop()
-print(*result)
+arr.sort()
+print(arr)
+for i in arr:
+    for j in range(K, -1, -1):
+        if i[0] + j < K+1:
+            count[i[0] + j] = max(i[1]+count[j], count[i[0] + j])
+    print(count)
+print(max(count))

4d448a1871e8a10848d5259be0be296673ba0feb
HOONTP <dltkdgns0303@gmail.com>
2023-09-06 17:58:49 +0900

    ssafy

--- bj quiz/bj 11401_조합모듈러.py
+++ bj quiz/bj 11401_조합모듈러.py
@@ -11,7 +11,7 @@ for i in range(M):
     top_value %= mod_
     bottom_value *= i + 1
     bottom_value %= mod_
-print(top_value)
-print(bottom_value)
+# print(top_value)
+# print(bottom_value)
 result = top_value * pow(bottom_value, -1, mod_) % mod_ # 파이썬 버전이 pow에 영향이 있는 듯.
 print(result)

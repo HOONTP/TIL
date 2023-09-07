b088a4f4a999b7699bfef33b239a0267d441b040
HOONTP <dltkdgns0303@gmail.com>
2023-09-07 18:00:15 +0900

    ssafy

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,4 +1,28 @@
 import sys
 input = sys.stdin.readline
 
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2  # 중간 인덱스 계산
 
+        if arr[mid] == target:
+            return mid  # 원하는 값이 발견됨
+        elif arr[mid] < target:
+            left = mid + 1  # 중간 값보다 오른쪽에 위치
+        else:
+            right = mid - 1  # 중간 값보다 왼쪽에 위치
+
+    return -1  # 원하는 값이 리스트에 없음
+
+N, C = map(int,input().split())
+lst = []
+for _ in range(N):
+    lst.append(int(input()))
+
+print(lst)
+#
+# if result != -1:
+#     print(f"7은 인덱스 {result}에서 찾았습니다.")
+# else:
+#     print("7을 찾을 수 없습니다.")

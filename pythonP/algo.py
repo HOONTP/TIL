4525eb3bc92ab2ebdf34e367d5a695b82a29717f
HOONTP <dltkdgns0303@gmail.com>
2023-09-12 17:59:56 +0900

    ssafy

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,28 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search():
-    start = 1
-    end = N * N
-
-    while start <= end:
-        mid = (start + end) // 2  # 중간 인덱스 계산
-        cnt = 0
-        for i in range(1, N+1):
-            if mid < i*N:
-                cnt += mid // i
-            else:
-                cnt += N
-        if cnt >= K:
-            end = mid - 1
-        else:
-            start = mid + 1
-
-    return start
-
-
 N = int(input())
-K = int(input())
-
-result = binary_search()
-print(result)
+arr = [list(map(int, input().split())) for _ in range(N)]
+print(arr)

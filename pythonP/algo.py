d74fd283c3af5ff16c057f75916fb21151e669bc
HOONTP <dltkdgns0303@gmail.com>
2023-09-19 00:29:42 +0900

    home

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,21 +1,21 @@
 import sys
 input = sys.stdin.readline
+import pprint
+def solve():
+    N, A = int(input()), [0] + list(map(int, input().split()))
+    # S[i]는 1번부터 i번까지의 누적합
+    S = [0 for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        S[i] = S[i - 1] + A[i]
 
-T = int(input())
+    # DP[i][j] : i에서 j까지 합하는데 필요한 최소 비용
+    # DP[i][k] + DP[k+1][j] + sum(A[i]~A[j])
+    DP = [[0 for i in range(N + 1)] for _ in range(N + 1)]
+    for i in range(2, N + 1):  # 부분 파일의 길이
+        for j in range(1, N + 2 - i):  # 시작점
+            DP[j][j + i - 1] = min([DP[j][j + k] + DP[j + k + 1][j + i - 1] for k in range(i - 1)]) + (S[j + i - 1] - S[j - 1])
 
-for tc in range(1, T+1):
-    price = list(map(int, input().split()))
-    month = list(map(int, input().split()))
-    DP = [0]*12
-
-    mn = price[3]
-    for i in range(12):
-        day = DP[i-1] + price[0]*month[i]
-        one = DP[i-1] + price[1]
-        three = DP[i-3] + price[2]
-
-        DP[i] = min(mn, day, one, three)
-        if DP[i] == mn:
-            DP[-1] = mn
-            break
-    print(f'#{tc}', DP[-1])
+    print(DP[1][N])
+    pprint.pprint(DP)
+for _ in range(int(input())):
+    solve()

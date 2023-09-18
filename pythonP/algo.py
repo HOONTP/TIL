5ed0580d8b579f987d0d5d1badbb6aee30e62c2d
HOONTP <dltkdgns0303@gmail.com>
2023-09-18 17:58:49 +0900

    ssafy

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,34 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def binary(arr, n):
-    start = 0
-    end = s_end - 1
+def solve():
+    N, A = int(input()), [0] + list(map(int, input().split()))
+    # S[i]는 1번부터 i번까지의 누적합
+    S = [0 for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        S[i] = S[i - 1] + A[i]
 
-    while start <= end:
-        mid = (start + end) // 2
-        if arr[mid] < n:
-            start = mid + 1
-        elif arr[mid] > n:
-            end = mid - 1
-        else:
-            return -1
-    return start
+    # DP[i][j] : i에서 j까지 합하는데 필요한 최소 비용
+    # DP[i][k] + DP[k+1][j] + sum(A[i]~A[j])
+    DP = [[0 for i in range(N + 1)] for _ in range(N + 1)]
+    for i in range(2, N + 1):  # 부분 파일의 길이
+        for j in range(1, N + 2 - i):  # 시작점
+            DP[j][j + i - 1] = min([DP[j][j + k] + DP[j + k + 1][j + i - 1] for k in range(i - 1)]) + (
+                        S[j + i - 1] - S[j - 1])
 
+    print(DP[1][N])
 
-N = int(input())
-
-lst = list(map(int, input().split()))
-stack = [lst[0]] # stack을 떠올렸지만 스택으로 쓰진 않음.
-s_end = 1
-
-for i in lst:
-    if stack[-1] < i:
-        s_end += 1
-        stack.append(i)
-    elif stack[-1] > i:
-        change = binary(stack, i)
-        if change != -1:
-            stack[change] = i
-
-print(s_end)
+for _ in range(int(input())):
+    solve()

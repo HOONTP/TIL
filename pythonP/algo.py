f545956db1c3b305cd4a473e04f9cb63a5ad8131
HOONTP <dltkdgns0303@gmail.com>
2023-09-19 17:50:19 +0900

    ssafy

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,41 +1,35 @@
 import sys
 input = sys.stdin.readline
-import pprint
-def solve():
-    N, A = int(input()), [0] + list(map(int, input().split()))
-    # S[i]는 1번부터 i번까지의 누적합
-    S = [0 for _ in range(N + 1)]
-    for i in range(1, N + 1):
-        S[i] = S[i - 1] + A[i]
+sys.setrecursionlimit(1000000000)
+di = [-1,1,0,0]
+dj = [0,0,-1,1]
 
-<<<<<<< HEAD
-def solve():
-    N, A = int(input()), [0] + list(map(int, input().split()))
-    # S[i]는 1번부터 i번까지의 누적합
-    S = [0 for _ in range(N + 1)]
-    for i in range(1, N + 1):
-        S[i] = S[i - 1] + A[i]
+def backT(x, y, visted):
+    global cnt
 
-    # DP[i][j] : i에서 j까지 합하는데 필요한 최소 비용
-    # DP[i][k] + DP[k+1][j] + sum(A[i]~A[j])
-    DP = [[0 for i in range(N + 1)] for _ in range(N + 1)]
-    for i in range(2, N + 1):  # 부분 파일의 길이
-        for j in range(1, N + 2 - i):  # 시작점
-            DP[j][j + i - 1] = min([DP[j][j + k] + DP[j + k + 1][j + i - 1] for k in range(i - 1)]) + (
-                        S[j + i - 1] - S[j - 1])
+    if x == M-1 and y == N-1:
+        for i in visted:
+            can_lst[i[0]][i[1]] += 1
+        cnt += 1
+        return
+    if can_lst[x][y] > 0:
+        for i in visted:
+            can_lst[i[0]][i[1]] = can_lst[x][y]
+        cnt += can_lst[x][y]
+        return
 
-    print(DP[1][N])
+    for k in range(4):
+        ni = x + di[k]
+        nj = y + dj[k]
+        if 0<=ni<M and 0<=nj<N and arr[ni][nj] < arr[x][y] and (ni,nj) not in visted:
+            visted.add((ni,nj))
+            backT(ni, nj, visted)
+            visted.remove((ni, nj))
 
-=======
-    # DP[i][j] : i에서 j까지 합하는데 필요한 최소 비용
-    # DP[i][k] + DP[k+1][j] + sum(A[i]~A[j])
-    DP = [[0 for i in range(N + 1)] for _ in range(N + 1)]
-    for i in range(2, N + 1):  # 부분 파일의 길이
-        for j in range(1, N + 2 - i):  # 시작점
-            DP[j][j + i - 1] = min([DP[j][j + k] + DP[j + k + 1][j + i - 1] for k in range(i - 1)]) + (S[j + i - 1] - S[j - 1])
+M, N = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(M)]
+can_lst = [[0]*N for _ in range(M)]
 
-    print(DP[1][N])
-    pprint.pprint(DP)
->>>>>>> d74fd283c3af5ff16c057f75916fb21151e669bc
-for _ in range(int(input())):
-    solve()
+cnt = 0
+backT(0,0,{(0,0)})
+print(cnt)

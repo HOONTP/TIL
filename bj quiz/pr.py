7c877f4b7ea76202238b85608db17310c165ca5d
HOONTP <dltkdgns0303@gmail.com>
2023-09-23 01:32:04 +0900

    home

--- bj quiz/pr.py
+++ bj quiz/pr.py
@@ -1,35 +1,30 @@
-import heapq
+import sys
+input = sys.stdin.readline
 
-def dik(node, graph):
-    q = []
-    heapq.heappush(q, (0, node))
+def sepa(s, e, n):
+    one = '1'*n
+    zero = '0'*n
+    for i in range(n):
+        if arr[s+i][e:e+n] != one:
+            break
+    else:
+        return '1'
+    for i in range(n):
+        if arr[s+i][e:e+n] != zero:
+            break
+    else:
+        return '0'
 
-    lst = [float('inf')]*(V+1)
-    lst[node] = 0
-    lst[0] = 0
-    visited = [0]*(V+1)
+    S = s + n // 2
+    E = e + n // 2
+    m = n // 2
+    return '(' + sepa(s, e, m) + sepa(s, E, m) + sepa(S, e, m) + sepa(S, E, m) + ')'
 
-    while q:
-        _, n = heapq.heappop(q)
 
-        for i, w in graph[n]:
-            if lst[n] + w < lst[i] and visited[i] == 0:
-                lst[i] = lst[n] + w
-                heapq.heappush(q, (lst[i], i))
-    for i in range(V+1):
-        value[i] += lst[i]
+    pass
+N = int(input())
 
-for t in range(1, int(input())+1):
-    V, E, X = map(int, input().split())
-    arr = [[] for _ in range(V+1)]
-    reverse = [[] for _ in range(V+1)]
-    for _ in range(E):
-        e, s, w = map(int, input().split())
-        arr[e].append((s, w))
-        reverse[s].append((e, w))
+arr = [input() for _ in range(N)]
 
-    value = [0]*(V+1)
-    dik(X, reverse)
-    dik(X, arr)
-
-    print(f'#{t}', max(value))
+result = sepa(0, 0, N)
+print(result)

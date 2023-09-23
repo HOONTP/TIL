18fbb8b87c32e0882d6e13cb09091468823b06d0
HOONTP <dltkdgns0303@gmail.com>
2023-09-24 01:23:24 +0900

    home

--- bj quiz/bj 1780_분할정복.py
+++ bj quiz/bj 1780_분할정복.py
@@ -11,16 +11,16 @@ def sepa(x, y, n):
                     for l in range(3):
                         sepa(x + dl * k, y + dl * l, dl)
                 return
-    if before == '-1':
+    if before == -1:
         counting[0] += 1
-    elif before == '0':
+    elif before == 0:
         counting[1] += 1
-    elif before == '1':
+    elif before == 1:
         counting[2] += 1
 
 
 N = int(input())
-arr = [list(input().split()) for _ in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
 counting = [0, 0, 0]
 
 sepa(0, 0, N)

32b6c4097ead9163f5eef081f540cd2313ae2552
HOONTP <dltkdgns0303@gmail.com>
2023-09-28 14:35:24 +0900

    home

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,33 +1,11 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
+# sys.setrecursionlimit(10**9)
 
-def find(x):
-    if x == dic_[x]:
-        return x
-    dic_[x] = find(dic_[x])
-    return dic_[x]
+A, B, C = map(int, input().split())
 
-def uniset(a, b):
-    A = find(a)
-    B = find(b)
-    if A != B:
-        dic_[A] = B
-        return 0
-    else:
-        return 1
-
-def prt():
-    for _ in range(M):
-        a, b = map(int, input().split())
-        same = uniset(a, b)
-        # 두개의 부모가 같은 순간 사이클이 형성된다.
-        if same:
-            return _+1
-    return 0
-
-N, M = map(int, input().split())
-dic_ = list(range(N))
-
-result = prt()
-print(result)
+if B >= C:
+    print(-1)
+else:
+    result = (A // (C - B)) + 1
+    print(result)

c0effd0541c0f93de262c6241e2b77c33b961522
HOONTP <dltkdgns0303@gmail.com>
2023-10-05 21:09:27 +0900

    home

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -1,15 +1,33 @@
 import sys
 input = sys.stdin.readline
+import heapq
 
-N = int(input())
+def bellman_ford(start):
+    distance = [float("inf")] * (V + 1)
+    distance[start] = 0
 
-stack = []
-size = 0
-sums = 0
+    for _ in range(V - 1):
+        for u, v, weight in edges:
+            if distance[u] + weight < distance[v]:
+                distance[v] = distance[u] + weight
 
-for _ in range(N):
-    x, r = map(int, input().split())
-    sums += 1
-    if size == 2:
-        if stack[0] == x-r and stack[-1] == x+r:
-            sums += 1
+    for u, v, weight in edges:
+        if distance[u] + weight < distance[v]:
+            print("-1")
+            return
+
+    # 결과 출력
+    for i in range(2, V+1):
+        if distance[i] < float("inf"):
+            print(f"{distance[i]}")
+        else:
+            print(-1)
+
+V, E = map(int, input().split())
+edges = []
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edges.append((u, v, w))
+
+start_vertex = 1
+bellman_ford(start_vertex)

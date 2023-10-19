a69edfadcf543f8e92f9bda10efc9b8caa984772
HOONTP <dltkdgns0303@gmail.com>
2023-10-19 22:03:53 +0900

    home

--- pythonP/main.py
+++ pythonP/main.py
@@ -1,46 +1,23 @@
 import sys
 input = sys.stdin.readline
-# sys.setrecursionlimit(10**9)
-import heapq
+sys.setrecursionlimit(10**9)
 
-def dijk(A, B):
-    q = []
-    heapq.heappush(q, (0, A))
-    visited = [float('inf')] * (N+1)
-    visited[A] = 0
-    load[A] = A
+def find(x):
+    if dic_[x] == x:
+        return x
+    dic_[x] = find(dic_[x])
+    return dic_[x]
 
-    while q:
-        w, n = heapq.heappop(q)
-        # print(log)
-        print(load)
-        print(visited)
-        if n == B:
-            return w
-        for weight, node in arr[n]:
-            sums = w+weight
-            if sums < visited[node]:
-                visited[node] = sums
-                load[node] = n
-                heapq.heappush(q, (sums, node))
+def unionset(a, b):
+    A = find(a)
+    B = find(b)
 
-N = int(input())
-M = int(input())
+    if A == B:
+        dic_[A] = 0
+    elif A == 0:
+        dic_[B] = 0
+    elif B == 0:
+        dic_[A] = 0
+    else:
+        dic_[A] = B
 
-arr = [[] for _ in range(N+1)]
-load = [0] * (N + 1)
-
-for _ in range(M):
-    a, b, W = map(int, input().split())
-    arr[a].append((W, b))
-A, B = map(int, input().split())
-
-result = dijk(A, B)
-print(load)
-print(result[0])
-print(len(result[1]))
-print(*result[1])
-
-# 최소 비용
-# 들르는 개수 - 무시
-# 도시 번호 순서대로

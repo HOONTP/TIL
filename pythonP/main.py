ac8ae68f2ab7f025381ffb49ef5e3ed97856ecfa
HOONTP <dltkdgns0303@gmail.com>
2023-10-20 17:58:50 +0900

    ssafy

--- pythonP/main.py
+++ pythonP/main.py
@@ -1,29 +1,32 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**4)
+sys.setrecursionlimit(10**6)
 
 #프리 - 루좌우
 #인오더 - 좌루우
 #포스트 - 좌우루
-def Pre_order(inorder, s, e):
-    if not inorder:
-        return []
-    # print(inorder, Postorder, s, e)
-    root = Postorder[e]
-    mid = inorder.index(root)
+def Pre_order(si, ei, so, eo):
+    if si > ei:
+        return
+    root = Postorder[eo]
+    mid = pos[root]
+    # print(root, Inorder, Postorder, si, ei, so, eo)
+    diff = ei - mid
 
-    left_in = inorder[:mid]
-    right_in = inorder[mid+1:]
+    print(root, end=' ') # 5 - 1 - 1
+    Pre_order(si, mid - 1, so, eo - diff - 1)
+    Pre_order(mid + 1, ei , eo - diff , eo - 1)
 
-    left_ = Pre_order(left_in, s, s+mid-1)
-    right_ = Pre_order(right_in, s+mid, e-1)
-
-    return [root] + left_ + right_
 
 
 N = int(input())
 Inorder = list(map(int, input().split()))
 Postorder = list(map(int, input().split()))
+pos=[0]*(N+1)
 
-result = Pre_order(Inorder, 0, N-1)
-print(*result)
+#리스트의 인덱스를 미리 저장해두는 / 해당 값의 인덱스 위치에 그 리스트의 인덱스를 저장
+for i in range(N):
+    pos[Inorder[i]]=i
+
+Pre_order(0, N-1, 0, N-1)
+# print(*result)

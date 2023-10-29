2be7d4147bd7fa36851b1d82f5fad38d51f7c8a7
HOONTP <dltkdgns0303@gmail.com>
2023-10-30 00:50:25 +0900

    home

--- pythonP/algo.py
+++ pythonP/algo.py
@@ -2,42 +2,9 @@ import sys
 input = sys.stdin.read
 sys.setrecursionlimit(10**9)
 
-class Tree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+N = int(input())
+M = int(input())
+arr = [[] for _ in range(N+1)]
+for _ in range(M):
 
-
-def build(preorder):
-    if not preorder:
-        return None
-
-    root_value = preorder[0]
-    root = Tree(root_value)
-
-    if len(preorder) > 1:
-        index = 1
-        while index < len(preorder) and preorder[index] <= root_value:
-            index += 1
-
-        root.left = build(preorder[1:index])
-        root.right = build(preorder[index:])
-    return root
-
-def SearchTree(root):
-    if root is None:
-        return []
-    SearchTree(root.left)
-    SearchTree(root.right)
-    print(root.value)
-
-
-In = input().strip()
-arr = list(map(int, In.split()))
-# print(arr)
-root = build(arr)
-# print(root)
-result = SearchTree(root)
-# for i in result:
-#     print(i)
+    pass

57aec56edbc9862c339f2652dff23898c86cdd42
HOONTP <dltkdgns0303@gmail.com>
2024-03-12 22:07:56 +0900

    가장 큰 수

--- programm/가장큰수.py
+++ programm/가장큰수.py
@@ -1,13 +1,14 @@
+## 내 무식한 코드
 def solution(numbers):
     # 646 64(5)  -> 646 64
     # 464 46(5)  -> 46 464
     # 322 32(2)  -> 32 322
     # 244 24(4)  -> 244 24 1번이 2번보다 작으면 큰수순?
     # 58 5 58 5
-    # 
-    # 445 
+    #
+    # 445
     # numbers = [100, 999, 989, 9, 98, 988, 0, 85, 8, 8, 586, 5, 58, 586, 576, 556, 554,646, 64, 464, 46,1000, 1, 15, 23, 32, 322, 232]
-    # 33 3 
+    # 33 3
 
     num_lst = [[[[] for _ in range(10)] for _ in range(10)] for _ in range(10)]
 
@@ -44,3 +45,26 @@ def solution(numbers):
     if int(answer) == 0:
         answer = '0'
     return answer
+
+
+
+#### 풀이 1
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key=lambda x: x*3, reverse=True)
+#     return str(int(''.join(numbers)))
+
+
+#### 풀이 2 이거 comparator 를 양수 음수가 아닌 하나의 지점을 기준으로 쫙 줄을 세우면 좀 더 복잡도가 줄어들지 않을까?
+# import functools
+#
+# def comparator(a,b):
+#     t1 = a+b
+#     t2 = b+a
+#     return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+#
+# def solution(numbers):
+#     n = [str(x) for x in numbers]
+#     n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+#     answer = str(int(''.join(n)))
+#     return answer

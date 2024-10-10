eb3e1e37a6d1fa6e192de20143daaf7236e78d41
HOONTP <139415963+HOONTP@users.noreply.github.com>
2024-10-10 15:06:34 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+ # 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일을 조회
+ # 댓글 작성일을 기준으로 오름차순 정렬해주시고, 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 
+SELECT A.TITLE, A.BOARD_ID, B.REPLY_ID, B.WRITER_ID, B.CONTENTS, DATE_FORMAT(B.CREATED_DATE, "%Y-%m-%d") AS CREATED_DATE
+FROM USED_GOODS_BOARD A
+INNER JOIN USED_GOODS_REPLY B ON A.BOARD_ID = B.BOARD_ID
+WHERE "2022-10-01" <= A.CREATED_DATE AND A.CREATED_DATE < "2022-11-01"
+ORDER BY B.CREATED_DATE ASC, A.TITLE ASC 
+;
